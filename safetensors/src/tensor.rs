596f0c9310f00a6a1326c47cdc00c16b69e44f93
Luc Georges <luc.sydney.georges@gmail.com>
2026-09-08 15:14:40 +0200

    feat: bump deps & MSRV
    Signed-off-by: Luc Georges <luc.sydney.georges@gmail.com>

--- safetensors/src/tensor.rs
+++ safetensors/src/tensor.rs
@@ -663,12 +663,13 @@ impl Metadata {
         Ok(start)
     }
 
-    /// [`TensorInfo`] list is guaranteed to be ordered by in buffer offset thanks to `TryFrom` sorting tensors by offset and [`Self::new`] calling [`Self::validate`]
+    /// [`TensorInfo`]s in data-offset order: contiguous and non-overlapping, enforced by
+    /// `validate` for every `Metadata`. [`Self::tensor_idx`] returns positions into this slice.
     pub fn tensor_infos(&self) -> &[TensorInfo] {
         &self.tensors
     }
 
-    /// get the index of a given tensor in the [`Self::tensors`] array
+    /// Position of `name` in [`Self::tensor_infos`], or `None` if absent.
     pub fn tensor_idx(&self, name: &str) -> Option<usize> {
         self.index_map.get(name).copied()
     }
